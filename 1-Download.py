48cdc2d46c002565fbbcc3a6531c7a44927a97cc
James Maze <jmaze@uoregon.edu>
2023-11-07 16:28:01 -0800

    Starting projects, now I just gotta finish them

---
+++ 1-Download.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  7 16:22:06 2023
+
+IceSat2 download script
+
+@author: jmaze
+"""
+
+# %% Libraries
+# ----------------------------------------------------------------------------
+
+import icepyx as ipx
+
