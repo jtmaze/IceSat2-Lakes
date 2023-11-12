ca7ba0390a153b0f5e7677b40118a57be3a2246a
jtmaze <jmaze@uoregon.edu>
2023-11-11 17:30:16 -0800

    why the heck is to_crs not working

--- 3-Lakes-IceSat2-merge.py
+++ 3-Lakes-IceSat2-merge.py
@@ -11,7 +11,9 @@ Created on Wed Nov  8 08:56:33 2023
 # ============================================================================
 
 import geopandas as gpd
+import pandas as pd
 import fiona
+import shapely
 import pprint
 
 # !!! Change this line for different local machines
@@ -46,9 +48,10 @@ fiona.drvsupport.supported_drivers['LIBKML'] = 'r'
 bound_box = gpd.read_file(data_raw + 'study_bounds.kml')
 print(bound_box.crs)
 
+
 # Bound box was imported with EPSG:4326, need to reassign
-bound_box = bound_box.to_crs(crs = crs_proj)
-bound_box = bound_box['geometry']
+bound_box = bound_box['geometry'].to_crs(crs = crs_proj)
+bound_box_geom = bound_box['geometry']
 print(bound_box.crs)
 
 # %%% 2.3 Filter the lakes dataset based on the project boundary
@@ -59,14 +62,28 @@ gr_lakes_filtered = gpd.clip(gr_lakes, bound_box)
 # Clean up variable explorer
 del(gr_lakes, bound_box)
 
-# %% 3. Import the IceSat2 data
+# %% 3. Import the IceSat2 data and make it a geopandas object
 # ----------------------------------------------------------------------------
 # ============================================================================
 
+icesat = pd.read_csv(data_intermediate + 'IceSat2_DataFrame.csv')
+
+geometry = [shapely.Point(lon, lat) for lon, lat in zip(icesat['lon'], icesat['lat'])]
+
+import geopandas as gpd
+
+icesat_points = gpd.GeoDataFrame(icesat, geometry = geometry)
+
+icesat_points = icesat_points.set_crs(crs = 'EPSG:4326')
+
+icesat_points = icesat_points.to_crs(crs = 'EPSG:32624')
 
                 
-            
-
+# %% 3. Import the IceSat2 data
+# ----------------------------------------------------------------------------
+# ============================================================================
+         
+icesat_filtered = gpd.clip(icesat_points, gr_lakes)
 
 
 
