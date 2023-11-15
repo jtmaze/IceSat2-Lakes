0efd14850dd798bee6db0a0a20d9eba5c7587348
James Maze <jmaze@uoregon.edu>
2023-11-15 14:24:30 -0800

    Added season observed category. Colored histogram plots.

--- 4-prelim-analysis.py
+++ 4-prelim-analysis.py
@@ -42,7 +42,7 @@ lake_pts_icesat['LakeID'] = lake_pts_icesat['LakeID'].astype(str)
 
 # Per this documentation:
 # https://nsidc.org/sites/default/files/icesat2_atl06_data_dict_v003_0.pdf
-# delta_time is seconds since 2018-01-01
+# delta_time is the number of seconds since 2018-01-01
 
 
 # Writing a function and using the .apply method should make this faster for large data. 
@@ -61,6 +61,30 @@ def calendar_from_delta (delta_time):
 # Run the function on the DataFrame. 
 lake_pts_icesat = lake_pts_icesat.assign(obs_date = lake_pts_icesat['delta_time'].apply(calendar_from_delta))
 
+# %%% Make a column to designate lake phase
+
+def lake_phaser (obs_date):
+    # !!! Check with Johnny on this approximate designation
+    szn_dict = {'frozen': [11, 12, 1, 2, 3, 4], # Nov thru April are frozen?
+                'intermediate': [5, 10], # May and Oct are intermediate?
+                'liquid': [6, 7, 8, 9]} # June, July, August and September are liquid?
+    
+    month = obs_date.month # Extract the month from the datetime object. 
+    
+    # Designate lake_phase_est based on szn_dict
+    if month in szn_dict['frozen']:
+        lake_phase_est = 'frozen'
+    if month in szn_dict['intermediate']:
+        lake_phase_est = 'intermediate'
+    if month in szn_dict['liquid']:
+        lake_phase_est = 'liquid'
+        
+
+    return(lake_phase_est)
+
+# Run the function on the dataframe
+lake_pts_icesat = lake_pts_icesat.assign(lake_phase_est = lake_pts_icesat['obs_date'].apply(lake_phaser))
+
 # %%% 2.3 Summarize IceSat data by lake
 
 # Generate an interesting summary table for each lake
@@ -70,7 +94,7 @@ summary1 = lake_pts_icesat.groupby('LakeID').agg({'height': ['std', 'mean'],
                                                   'obs_date': [lambda x: x.unique().astype(str).tolist(), # Get a list of unique obs dates
                                                                lambda x: len(x.unique().astype(str).tolist())] # Counts each unique obs date
                                                   })
-# Since there's a LakeID column, reset the index
+# Reset the index to make LakeID a simple column
 summary1.reset_index(inplace = True)
 
 # Make the column names more legible
@@ -80,10 +104,11 @@ summary1.columns = ['LakeID', 'lake_height_std', 'lake_height_mean', 'lake_area'
 
 # %%% 2.4 Query for lakes w. robust data
 
-# Filter lakes that don't have ridiculously high std?
+# Query lakes that don't have ridiculously large height stdv
+# Also get lakes with a sizeable observation count
 summary1_robust = summary1.query('lake_height_std < 30 & lake_observation_count > 25')
 
-# %%% 2.5 Visualize summary stats
+# %%% 2.5 Visualize summary stats for 'robust' and original datasets
 
 # Relationship between observation count and height_std?
 summary1.plot.scatter(x = 'lake_observation_count', y = 'lake_height_std')
@@ -102,10 +127,10 @@ summary1_robust.plot.scatter(x = 'lake_area', y = 'unique_dates_count')
 # ----------------------------------------------------------------------------
 # ============================================================================
 
-# Isolate the best lakes from orgininal data
+# Isolate the best lakes from orgininal IceSat data
 robust_lake_pts = lake_pts_icesat[lake_pts_icesat['LakeID'].isin(summary1_robust['LakeID'])]
 
-# Join info from robust summary to the lake pts. 
+# Join info from robust summary table to the Icesat lake pts. 
 robust_lake_pts = pd.merge(robust_lake_pts, 
                             summary1_robust, 
                             how = 'left', 
@@ -113,37 +138,44 @@ robust_lake_pts = pd.merge(robust_lake_pts,
 
 # Make new column for range from mean for each value
 robust_lake_pts['diff_from_mean'] = (robust_lake_pts['height'] - robust_lake_pts['lake_height_mean'])
+            
 
 #!!! Filter lake pts based on the distance from the mean
 robust_lake_pts = robust_lake_pts.query('-100 < diff_from_mean < 100')
 
 # Subset the data by LakeID for plotting
-subset_LakeIDs = summary1_robust['LakeID'].iloc[36:42]
+subset_LakeIDs = summary1_robust['LakeID'].iloc[36:52]
 subset_lake_pts = robust_lake_pts[robust_lake_pts['LakeID'].isin(subset_LakeIDs)]
 
 
 # Make the figure
 fig = plt.figure(figsize=[12, 8])
 # Designate number of cols and rows
-rows = 3
-cols = 2
+rows = 4
+cols = 4
+colors_dict = {'frozen': 'blue', 'intermediate': 'green', 'liquid': 'red'}
 
 for i, lake_id in enumerate(subset_LakeIDs):
     plt.subplot(rows, cols, i + 1)
     # Match data to current Lake_ID
     dataplot = robust_lake_pts[robust_lake_pts['LakeID'] == lake_id]
-    # Generate a histogram
-    plt.hist(dataplot['diff_from_mean'], bins = 50)
+    # Get lake phases for each LakeID
+    lake_phases = dataplot['lake_phase_est'].unique()
+    # Itereate through lake phases and plot color bars
+    for lake_phase in lake_phases:
+        phase_data = dataplot[dataplot['lake_phase_est'] == lake_phase]['diff_from_mean']
+        # Generate a histogram
+        plt.hist(phase_data, bins = 25, 
+                 color = colors_dict.get(lake_phase, 'grey')
     # Plot title
     plt.title(f'Lake ID = {lake_id}')
+    
 
 plt.tight_layout()
 plt.show()
 
-# Clean up variable explorer
-
     
-# %% 3. Plot the distributions of altimeter measurements for the different lakes
+# %% 3. Make maps of some lakes. 
 # ----------------------------------------------------------------------------
 # ============================================================================   
     
